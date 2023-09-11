20a7f841b6825d351dee0b0031d85673694962e5
lanceajiro <142672718+lanceajiro@users.noreply.github.com>
2023-09-11 23:14:35 +0800

    Create cdp.js

---
+++ cdp.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+const fs = require("fs-extra");
+
+const cacheDirectory = `${__dirname}/../temp/`;
+
+async function cdp(event, api) {
+  const input = event.body.toLowerCase();
+
+  if (input.includes('-help')) {
+    const usage = "Usage: cdp\n\n" +
+      "Description: Get a couple display picture (DP).\n\n" +
+      "Example: cdp\n\n" +
+      "Note: This command provides a random couple DP.";
+    api.sendMessage(usage, event.threadID, event.messageID);
+    return;
+  }
+
+  try {
+    const { data } = await axios.get(
+      "https://tanjiro-api.onrender.com/cdp?api_key=tanjiro"
+    );
+    const maleImg = await axios.get(data.male, { responseType: "arraybuffer" });
+    fs.writeFileSync(cacheDirectory + "img1.png", Buffer.from(maleImg.data, "utf-8"));
+    const femaleImg = await axios.get(data.female, { responseType: "arraybuffer" });
+    fs.writeFileSync(cacheDirectory + "img2.png", Buffer.from(femaleImg.data, "utf-8"));
+
+    const msg = "Here is your couple DP";
+    const allImages = [
+      fs.createReadStream(cacheDirectory + "img1.png"),
+      fs.createReadStream(cacheDirectory + "img2.png")
+    ];
+    
+    api.sendMessage({
+      body: msg,
+      attachment: allImages
+    }, event.threadID, event.messageID);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = cdp;
