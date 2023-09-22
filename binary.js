fc3f7ff67868254e8d36a203b92db6231ea2759d
lanceajiro <142672718+lanceajiro@users.noreply.github.com>
2023-09-22 17:16:29 +0800

    Update binary.js

--- binary.js
+++ binary.js
@@ -3,32 +3,45 @@ async function binary(event, api) {
 
   if (input.includes('-help')) {
     const usage = "Usage: binary [text]\n\n" +
-      "Description: Converts the provided text into binary.\n\n" +
-      "Example: binary lance";
+      "Description: Converts the provided text into binary or decodes binary into text.\n\n" +
+      "Example (encode): binary hello\nExample (decode): binary -decode 01101000 01100101 01101100 01101100 01101111";
     api.sendMessage(usage, event.threadID, event.messageID);
     return;
   }
 
-  const apiKey = "tanjiro"; // API key for the service
-  const textToConvert = input.split("binary")[1].trim();
+  if (input.includes('-decode')) {
+    const binaryCodeToConvert = input.split("-decode")[1].trim().split(' ');
 
-  let response;
-  try {
-    response = await fetch(`https://tanjiro-api.onrender.com/binary?text=${textToConvert}&api_key=${apiKey}`);
-  } catch (err) {
-    // Handle the fetch error here
-    api.sendMessage("⚠️ Failed to fetch data.", event.threadID, event.messageID);
-    return;
-  }
+    const decodeBinary = (binaryCode) => {
+      return binaryCode
+        .map(code => String.fromCharCode(parseInt(code, 2))) // Convert binary to character
+        .join(''); // Join characters to form text
+    };
 
-  const jsonResponse = await response.json();
-
-  if (jsonResponse.type === "success" && jsonResponse.result) {
-    const binaryText = jsonResponse.result;
-    await api.sendMessage(`${binaryText}`, event.threadID, event.messageID);
+    try {
+      const decodedText = decodeBinary(binaryCodeToConvert);
+      await api.sendMessage(`${decodedText}`, event.threadID, event.messageID);
+    } catch (err) {
+      console.error(err);
+      await api.sendMessage("⚠️ Failed to decode binary code.", event.threadID, event.messageID);
+    }
   } else {
-    // Handle the API response error here
-    api.sendMessage("⚠️ Failed to convert text to binary.", event.threadID, event.messageID);
+    const textToConvert = input.split("binary")[1].trim();
+
+    const convertToBinary = (text) => {
+      return text
+        .split('')
+        .map(char => char.charCodeAt(0).toString(2)) // Convert each character to binary
+        .join(' '); // Add space between binary digits
+    };
+
+    try {
+      const binaryText = convertToBinary(textToConvert);
+      await api.sendMessage(`${binaryText}`, event.threadID, event.messageID);
+    } catch (err) {
+      console.error(err);
+      await api.sendMessage("⚠️ Failed to convert text to binary.", event.threadID, event.messageID);
+    }
   }
 }
 
